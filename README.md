d7621b7126c7b20e4ef03ee99dc9edef1cd805b7
Leila Suleimanova <leila_suleymanova@mail.ru>
2023-09-15 03:01:59 +0300

    Добавляет npm пакет

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # movies-explorer-api
+
+IP 84.201.158.120
+
+Backend https://api.leila.nomoredomainsicu.ru
--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://api.leila.nomoredomainsicu.ru'],
   credentials: true,
 }));
 
